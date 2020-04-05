23b6121025803020e05d0ece107e49268fdb27d5
annacallahan <anna.r.callahan@gmail.com>
2020-04-05 10:24:06 -0500

    Up to part 5

--- Documents/LC101/HW6/orbit-report/src/app/app.component.ts
+++ Documents/LC101/HW6/orbit-report/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Satellite} from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+  sourceList: Satellite[];
+
+  constructor() {
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+    window.fetch(satellitesUrl).then(function(response){
+      response.json().then(function(data){
+        let fetchedSatellites = data.satellites;
+        let newSatellite = {};
+        for (let index = 0; index < fetchedSatellites.length; index++) {
+        newSatellite = new Satellite(fetchedSatellites[index].name, fetchedSatellites[index].type, fetchedSatellites[index].launchDate, fetchedSatellites[index].orbitType, fetchedSatellites[index].operational);
+        this.sourceList.push(newSatellite);
+        console.log(this.sourceList);
+        }
+      }.bind(this));
+
+    }.bind(this));
+  }
 }
